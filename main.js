ec59f56e46300cfec0c09aa3aab73eca1f7ef523
Jessica Fatta <jessicafatta@gmail.com>
2021-11-03 19:01:51 -0400

    Initial build of js file

---
+++ main.js
@@ -0,0 +1,15 @@
+var studyButton = document.getElementById("study");
+var meditateButton = document.getElementById("meditate");
+var exerciseButton = document.getElementById("exercise");
+
+
+studyButton.addEventListener("click", changeColor);
+meditateButton.addEventListener("click", changeColor);
+exerciseButton.addEventListener("click", changeColor);
+
+
+
+
+function changeColor() {
+
+}
