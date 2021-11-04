e3e8b5b311c42e48bd7c20a0d7666430f7e169bf
Jessica Fatta <jessicafatta@gmail.com>
2021-11-03 22:20:36 -0400

    Add button highlight

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
-var studyButton = document.getElementById("study");
-var meditateButton = document.getElementById("meditate");
-var exerciseButton = document.getElementById("exercise");
-
-
-studyButton.addEventListener("click", changeColor);
-meditateButton.addEventListener("click", changeColor);
-exerciseButton.addEventListener("click", changeColor);
-
-
-
-
-function changeColor() {
-
+var studyButton = document.querySelector(".study");
+var studyButtonActive = document.querySelector(".study-active");
+var meditateButton = document.querySelector(".meditate");
+var meditateButtonActive = document.querySelector(".meditate-active");
+var exerciseButton = document.querySelector(".exercise");
+var exerciseButtonActive = document.querySelector(".exercise-active");
+studyButton.addEventListener("click", activateStudy);
+meditateButton.addEventListener("click", activateMeditate);
+exerciseButton.addEventListener("click", activateExercise);
+function show(element) {
+  element.classList.remove("hidden");
+};
+function hide(element) {
+  element.classList.add("hidden");
+};
+function toggle(element) {
+  element.toggle("hidden");
+}
+function activateStudy(element) {
+  hide(studyButton);
+  show(studyButtonActive);
+}
+function activateMeditate(element) {
+  hide(meditateButton);
+  show(meditateButtonActive);
+}
+function activateExercise(element) {
+  hide(exerciseButton);
+  show(exerciseButtonActive);
 }
